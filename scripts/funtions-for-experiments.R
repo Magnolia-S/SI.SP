0e112e2ed5c77b2729c97c486bf38d9d4116f589
Rachel <rachel.sabatello@gmail.com>
2023-03-16 16:49:17 -0400

    Notes on Functions.R + resp info breakdown

--- scripts/funtions-for-experiments.R
+++ scripts/funtions-for-experiments.R
@@ -4,7 +4,7 @@ formatData <- function(.data, experiment) {
   require(lubridate)
   
   assert_that(
-    experiment %in% c("PRATTLIM"),
+    experiment %in% c("PRATTLIM"), ## is this the same for me?
     msg = "It looks like you're trying to process a new experiment. First edit formatData to accommodate that experiment.")
     
   .data %<>%
@@ -12,11 +12,14 @@ formatData <- function(.data, experiment) {
                assignmentid = experiment_id,
                ParticipantID = workerid) %>%
     
+    
+### Check this section!!
+------------------------------ 
     # Separate the practice, exposure, and test columns into one column per trial
     # (each with one more parts then there are trials because the last element is also followed by a ";". 
     # This last empty element should then be removed. If you get a warning, however, that means that 
     # at least one participant has more trials than expected)
-     { if ("practiceResp" %in% names(.))     
+     { if ("practice//Resp" %in% names(.))     
         separate(., 
                  practiceResp,
                  # Practice Trial = unlimted, should be no more than 4
@@ -51,31 +54,47 @@ formatData <- function(.data, experiment) {
     # Separate *trial-level* information into multiple columns
     separate(
       value,
-      into = c("random_id",
-               "participant_id",
-               "experiment_id", 
-               "Attended.Talker_Gender",
-               "Attended.Talker_Ear",
-               "Attended.Talker_Material",
-               "Attended.Talker_Label",
-               "Item.Filename", 
-               "Response", 
-               "PracticeResp",
-               "preloadResp",
-               "Word_recall",
-               "TestOrder",
-               "ExposureOrder",
-               "audio_type",
-               "audio_stall",
-               "Time.StartOfStimulus", 
-               "Time.EndOfTrial", 
-               "Response.RT",
-               "platform",
-               "comments"
+      into = c("Block_Number",
+               "Trial.Order",
+               "Stimulus",            ## Seems stimuli were given a number in the block
+               
+               "Filename",
+                  "Trial.Type",       ## Critical, Filler, Test
+               
+                  # I think I'll need to separate the file names into Talker.A and Talker.B
+                  # based on the ear (order in the file name), then mutate = Attended.Talker
+                  # based on Talker.X_Gender; TALKER.A and TALKER.B are PLACEHOLDERS
+                  
+                  "Talker.A_Label",   ## Critical: S/As, Sh/Ash; Filler: N/W
+                  "Talker.A_Shift",   ## only for Critical
+                  "Talker.A_Sound",
+                  "Talker.A_Ear",     ## Always L 
+                                                      ### NO Talker.B FOR TEST
+                  "Talker.B_Label",
+                  "Talker.B_Shift",   ## only for Critical
+                  "Talker.B_Sound",
+                  "Talker.B_Ear", 
+                
+               # (Not in file name)
+                # "Attended.Talker",  ## Attended.Talker Gender = L for Critical
+                # "Correct.resp",     ## defined by Talker.A_Gender + Attended.Talker 
+               
+                  "Resp.Key",         ## 77 vs 88; also have participant answer
+                  "Response",         ## Participant reported Word/Nonword
+              
+                 # 3 numbers that I think have to do with trial duration 
+               
+                  "#1",               ## start
+                  "#2",               ## end
+                  "#3",               ## elapsed (units = ms?)
+               
+                 # the word "false"; not sure in response to what
+                  "false"
                ),
-      sep = ",") %>%
+      
+            sep = ",") %>%
+------------------------------
     
-    ### ???
     # Add Experiment information
     mutate(Experiment = experiment) %>%
     
@@ -90,10 +109,13 @@ formatData <- function(.data, experiment) {
    
     # Make Trial numerical
     mutate(Trial = as.numeric(Trial)) %>%
+  
+
+### Check this section!!
+------------------------------
+    # Create block variable (1-10 for exposure, 1-12 for test) 
+      ## My trial-level data is already grouped by block?
     
-   
-     # Create block variable (1-10 for exposure, 1-12 for test)
-    ### My trial-level data is already grouped by block
     group_by(Phase) %>% 
     mutate(
       Phase = factor(tolower(Phase), levels = levels.Phase),
@@ -101,42 +123,29 @@ formatData <- function(.data, experiment) {
         cut(Trial, if (first(Phase) == "exposure") 8 else 6, labels = FALSE)) %>% # 8 and 6 should be replaced by the number of trials in each block of exposure and test, respectively
     ungroup() %>%
     mutate(
-
       
-### CHANGE !!!
-      
-    # remove .wav from the file name
-     Item = gsub("*.wav$", "", Item.Filename),
-     
-    # Item types
-    Critical.Item = 
-    
-    Filler.Item = 
-    
-    Test.Item =   
-     
-     
-      
-      # Extract item information
-       REMOVE.Item = ifelse(
+    # Extract item information <- this is original
+      REMOVE.Item = ifelse(
         grepl("\\-occluder", Item.Filename),
         gsub("^(.*)\\-occluder\\.(webm|mp4)$", "\\1", Item.Filename),
         gsub("^(.*)\\.(webm|mp4)$", "\\1", Item.Filename)),
-
       # All of the stuff below this line needs to be adjusted to parse your filename into info about this trial/item
-     
-     
-      
-      # Critical
-      Critical.Item = ("^Critical_\\.csv", Item.Filename)
-        
-      
-      # Filler
-      Filler.Item = grepl("^Filler_\\.csv", REMOVE.Item)
-      ifelse(Filler.Item grepl("W", Item), "mouth", "hand"),
     
-      # Test
-      Test.Item = ("^Test_\\.csv", REMOVE.Item)
+    # My edits
+      # remove .wav from the file name
+        Item = gsub("*.wav$", "", Item.Filename),
+     
+      # Item types
+        
+        # Critical
+        Critical.Item = grepl("^Critical_\\.csv", Item.Filename)
+        
+        # Filler
+        Filler.Item = grepl("^Filler_\\.csv", Item.Filename)
+          ifelse(Filler.Item grepl("W", Item), "mouth", "hand"),
+        
+        # Test
+        Test.Item = grepl("^Test_\\.csv", Item.Filename)
       
       Item.Type = ifelse(Phase == "test", "test", gsub("^([A-Z]+)[0-9]+.*$", "\\1", REMOVE.Item)),
       
@@ -161,7 +170,7 @@ formatData <- function(.data, experiment) {
         Task == "test" ~ "identification",
         T ~ NA_character_
       )) %>%
-###  
+------------------------------ 
     
     # Get key character based on Gevher's reading of the JS code (labelingBlock.js)
                       # (and make sure that "B" responses lead to NAs in the Response variable)
@@ -171,8 +180,10 @@ formatData <- function(.data, experiment) {
                           Response.Keycode == "77" ~ "M",
                           Response.Keycode == "88" ~ "X",
                           T ~ NA_character_),
-  
-### Change!!!                      
+
+                          
+### Check this section!!
+------------------------------                  
                          Response.CorrectWordStatus = case_when(
                            
                            item.type == "Critical" 
@@ -188,7 +199,7 @@ formatData <- function(.data, experiment) {
                           as.character(Response) == as.character(Item.WordStatus) ~ 1,
                           as.character(Response) != as.character(Item.WordStatus) ~ 0,
                           T ~ NA_real_)) %>%
-###                    
+------------------------------                 
     
                      # Add time information based on assignment submit time
                         { if ("userDateTimeOffset" %in% names(.)) 
